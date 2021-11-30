7204a1e8e4da4b8dae60db2555556e2e19188c4d
priyankap <priyanka123360@gmail.com>
2021-11-30 19:09:32 +0530

    Added : Mood Analyser Test for HAPPY

--- src/com/bridgelabz/MoodAnalyser_Test.java
+++ src/com/bridgelabz/MoodAnalyser_Test.java
@@ -6,21 +6,26 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MoodAnalyser_Test {
-	public String message = "I am in Sad Mood";
+	public String message = "I am in Happy Mood";
 	public static MoodAnalyser mood;
 
 	@BeforeClass
 	public static void init() {
-		
+
 		mood = MoodAnalyser.getInstance();
 	}
 
 	@Test
 	public void given_message_should_return_SAD() {
-		
 
 		String testMessage = mood.analyse_Mood(message);
 		assertEquals("SAD", testMessage);
 	}
 
+	@Test
+	public void given_message_should_return_HAPPY() {
+
+		String testMessage = mood.analyse_Mood(message);
+		assertEquals("HAPPY", testMessage);
+	}
 }
