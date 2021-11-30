d10da353ca76a71123c2af6f11366d67361315cd
priyankap <priyanka123360@gmail.com>
2021-11-30 18:53:03 +0530

    Added : Mood Analyser Test for SAD

---
+++ src/com/bridgelabz/MoodAnalyser_Main.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class MoodAnalyser_Main {
+	public static void main(String[] args) {
+//		MoodAnalyser mood = MoodAnalyser.getInstance();
+//		System.out.println("Enter Your Message.");
+//		Scanner sc = new Scanner(System.in);
+//		String message = sc.nextLine();
+//		mood.analyse_Mood(message);
+	}
+
+}
