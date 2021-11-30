1472250ca7f58d339a5ad3429f398160b3d41d8e
priyankap <priyanka123360@gmail.com>
2021-11-30 21:39:17 +0530

    Added : Handle Exception in Mood Analyser

--- src/com/bridgelabz/MoodAnalyser_Main.java
+++ src/com/bridgelabz/MoodAnalyser_Main.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class MoodAnalyser_Main {
 	public static void main(String[] args) {
-//		MoodAnalyser mood = MoodAnalyser.getInstance();
+		
 //		System.out.println("Enter Your Message.");
 //		Scanner sc = new Scanner(System.in);
-//		String message = sc.nextLine();
-//		mood.analyse_Mood(message);
+//		String message = null;
+		MoodAnalyser mood = new MoodAnalyser(null);
+		System.out.println(mood.analyse_Mood());
 	}
 
 }
