2d1fea513f2e65d8081e6f1f868d269c4314f3a3
priyankap <priyanka123360@gmail.com>
2021-11-30 21:11:44 +0530

    Refactor : Mood Analyser Refactor Test for HAPPY

--- src/com/bridgelabz/MoodAnalyser_Test.java
+++ src/com/bridgelabz/MoodAnalyser_Test.java
@@ -6,7 +6,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MoodAnalyser_Test {
-	public String message = "I am in Sad Mood";
+	public String message = "I am in happy Mood";
 	public static MoodAnalyser mood;
 
 	
@@ -22,11 +22,11 @@ public class MoodAnalyser_Test {
 		String testMessage = mood.analyse_Mood();
 		assertEquals("SAD", testMessage);
 	}
-//
-//	@Test
-//	public void given_message_should_return_HAPPY() {
-//
-//		String testMessage = mood.analyse_Mood(message);
-//		assertEquals("HAPPY", testMessage);
-//	}
+
+	@Test
+	public void given_message_should_return_HAPPY() {
+		mood =  new MoodAnalyser(message);
+		String testMessage = mood.analyse_Mood();
+		assertEquals("HAPPY", testMessage);
+	}
 }
